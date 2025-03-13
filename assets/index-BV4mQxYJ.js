52b5040980213b97921bf1133ba79c8b603c329f
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-13 17:08:15 +0900

    Updates

--- assets/index-BV4mQxYJ.js
+++ assets/index-BV4mQxYJ.js
@@ -52,34 +52,6 @@ function InitHeader() {
   const $headerContainer = document.querySelector(".gnb");
   Header($headerContainer);
 }
-function DetailModal(container, inputValue) {
-  container.innerHTML += `
-      <div class="restaurant-detail-modal-background">
-          <div class="restaurant-detail-modal">
-              <div class="restaurant-detail-modal-images">
-                  <div class="restaurant__category">
-                      <img src="./category-${inputValue.category}.png" alt="${inputValue.categoryValue}" class="category-icon"/>
-                  </div>
-                  <button class="restaurant-favorite-star-button">
-                    <img class="restaurant-favorite-star" src=${inputValue.favorite === false ? "./favorite-icon-lined.png" : "./favorite-icon-filled.png"} alt="favorite star"/>
-                  </button>
-              </div>
-              <div class="restaurant-detail-modal-info">
-                  <h3 class="restaurant__name text-subtitle">${inputValue.nameValue}</h3>
-                  <span class="restaurant__distance text-body">
-              캠퍼스부터 ${inputValue.distanceValue}분 내
-              </span>
-                  <p class="restaurant__detail__modal__distance text-body">${inputValue.descriptionValue}</p>
-                  <a href="${inputValue.link}">${inputValue.link}</a>
-              </div>
-              <div class="restaurant-detail-modal-buttons">
-                  <button class="restaurant-detail-modal-delete-button">삭제하기</button>
-                  <button class="restaurant-detail-modal-close-button">닫기</button>
-              </div>
-          </div>
-      </div>
-      `;
-}
 function AddNewRestaurant({ restaurant }) {
   if (!GetAllRestaurants()) {
     localStorage.setItem("restaurants", JSON.stringify([restaurant]));
@@ -108,6 +80,34 @@ function DeleteRestaurant(restaurantName) {
     (restaurant) => restaurant.nameValue !== restaurantName
   );
   localStorage.setItem("restaurants", JSON.stringify(filteredRestaurants));
+}
+function DetailModal(container, inputValue) {
+  container.innerHTML += `
+      <div class="restaurant-detail-modal-background">
+          <div class="restaurant-detail-modal">
+              <div class="restaurant-detail-modal-images">
+                  <div class="restaurant__category">
+                      <img src="./category-${inputValue.category}.png" alt="${inputValue.categoryValue}" class="category-icon"/>
+                  </div>
+                  <button class="restaurant-favorite-star-button">
+                    <img class="restaurant-favorite-star" src=${inputValue.favorite === false ? "./favorite-icon-lined.png" : "./favorite-icon-filled.png"} alt="favorite star"/>
+                  </button>
+              </div>
+              <div class="restaurant-detail-modal-info">
+                  <h3 class="restaurant__name text-subtitle">${inputValue.nameValue}</h3>
+                  <span class="restaurant__distance text-body">
+              캠퍼스부터 ${inputValue.distanceValue}분 내
+              </span>
+                  <p class="restaurant__detail__modal__distance text-body">${inputValue.descriptionValue}</p>
+                  <a href="${inputValue.link}">${inputValue.link}</a>
+              </div>
+              <div class="restaurant-detail-modal-buttons">
+                  <button class="restaurant-detail-modal-delete-button">삭제하기</button>
+                  <button class="restaurant-detail-modal-close-button">닫기</button>
+              </div>
+          </div>
+      </div>
+      `;
 }
 function SaveFavoriteRestaurant() {
   const $favoriteButtons = document.querySelectorAll(
@@ -185,6 +185,7 @@ function DeleteModalEvent() {
     );
     const $restaurantName = (_a = $detailModal == null ? void 0 : $detailModal.querySelector(".restaurant__name")) == null ? void 0 : _a.textContent;
     DeleteRestaurant($restaurantName);
+    alert(`${$restaurantName} 음식점이 삭제되었습니다.`);
     location.reload();
   });
 }
